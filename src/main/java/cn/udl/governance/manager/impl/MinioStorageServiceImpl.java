6bb10fe31be00ee17c656fe51b08e5752b991561
JX <1556390619@qq.com>
2026-02-04 11:29:33 +0800

    add Minio回调机制

--- src/main/java/cn/udl/governance/manager/impl/MinioStorageServiceImpl.java
+++ src/main/java/cn/udl/governance/manager/impl/MinioStorageServiceImpl.java
@@ -1,15 +1,19 @@
 package cn.udl.governance.manager.impl;
 
+import cn.udl.governance.common.ErrorCode;
+import cn.udl.governance.exception.BusinessException;
 import cn.udl.governance.manager.FileStorageService;
 import io.minio.*;
 import io.minio.http.Method;
 import jakarta.annotation.PostConstruct;
 import lombok.extern.slf4j.Slf4j;
+import okhttp3.ConnectionPool;
+import okhttp3.OkHttpClient;
 import org.springframework.beans.factory.annotation.Value;
-import org.springframework.boot.autoconfigure.condition.ConditionalOnProperty;
 import org.springframework.stereotype.Service;
 
 import java.io.InputStream;
+import java.util.concurrent.TimeUnit;
 
 @Slf4j
 @Service
@@ -34,6 +38,12 @@ public class MinioStorageServiceImpl implements FileStorageService {
         minioClient = MinioClient.builder()
                 .endpoint(endpoint)
                 .credentials(accessKey, secretKey)
+                .httpClient(new OkHttpClient.Builder()
+                        .connectTimeout(30, TimeUnit.SECONDS)
+                        .writeTimeout(300, TimeUnit.SECONDS) // 增大写超时
+                        .readTimeout(300, TimeUnit.SECONDS)
+                        .connectionPool(new ConnectionPool(10, 5, TimeUnit.MINUTES)) // 复用连接
+                        .build())
                 .build();
     }
 
@@ -45,13 +55,13 @@ public class MinioStorageServiceImpl implements FileStorageService {
             // 检查桶是否存在，不存在则创建
             boolean isExist = minioClient.bucketExists(BucketExistsArgs.builder().bucket(bucketName).build());
             if (!isExist) {
-            // 如果桶不存在，则创建新桶
+                // 如果桶不存在，则创建新桶
                 minioClient.makeBucket(MakeBucketArgs.builder().bucket(bucketName).build());
-            // 记录桶创建日志
+                // 记录桶创建日志
                 log.info("Created bucket: {}", bucketName);
             }
 
-        // 使用MinIO客户端上传对象到指定存储桶
+            // 使用MinIO客户端上传对象到指定存储桶
             minioClient.putObject(
                     PutObjectArgs.builder()
                             .bucket(bucketName)      // 指定存储桶名称
@@ -59,12 +69,13 @@ public class MinioStorageServiceImpl implements FileStorageService {
                             .stream(inputStream, inputStream.available(), -1)  // 设置输入流和大小
                             .contentType(contentType) // 设置内容类型
                             .build()                 // 构建参数
+
             );
+            log.info("MinIO store success {}", objectName);
         } catch (Exception e) {
-        // 记录错误日志
+            // 记录错误日志
             log.error("MinIO store failed", e);
-        // 抛出运行时异常，封装原始异常信息
-            throw new RuntimeException("MinIO storage error", e);
+            throw new BusinessException(ErrorCode.SYSTEM_ERROR);
         }
     }
 
